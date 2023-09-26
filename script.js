52278496f3737948ac1a7b19434b0c23aac056fd
sokar27 <cmm270494@gmail.com>
2023-09-26 11:05:05 -0300

    modified to use buttons and textcomment

--- script.js
+++ script.js
@@ -1,31 +1,118 @@
 let pcscore=0;
 let playerscore=0;
-for(let i=0;i<5;i++){
+let round=0;
+let choice;
+const selecpiedra=document.querySelector('#imagenpiedra');
+const selecpapel=document.querySelector('#imagenpapel');
+const selecttijera=document.querySelector('#imagentijera');
+const seleccuerpo=document.getElementById("cuerpo");
+const seleccoment=document.querySelector('#comentarios');
+
+
 function computerSelection (min,max){
    return (Math.floor(Math.random()*(max - min +1)+min));
    
    
 
 }
-function playmech (){
+
+
+
+
+selecpiedra.addEventListener('click',funsionpiedra);
+selecpapel.addEventListener('click',funsionpapel);
+selecttijera.addEventListener('click',funsiontijera);
+
+
+function funsionpiedra(){
+   let playerchoice='piedra';
+ playmech(playerchoice,choice);
+ fround();
+}
+
+
+function funsionpapel(){
+   let playerchoice='papel';
+   playmech(playerchoice,choice);
+   ;
+    fround();
+}
+
+
+function funsiontijera(){
+   let playerchoice='tijeras';
    
+    playmech(playerchoice,choice);
+    fround();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playmech (playerchoice,choice){
+    choice=computerSelection(1,3);
+    console.log(choice);
+    console.log(playerchoice);
+    let victoria
     
-     if ((playerChoice=='rock'&& choice==3)
-   ||(playerChoice=='paper'&& choice==1)||(playerChoice=='scisors'&& choice==2) ){
+     if ((playerchoice=='piedra'&& choice==3)
+   ||(playerchoice=='papel'&& choice==1)||(playerchoice=='tijeras'&& choice==2) ){
     console.log('you won!');
-    playerscore++;}
-    else if ((playerChoice=='rock'&&choice==1)
-    ||(playerChoice=='paper'&&choice==2)||(playerChoice==3&&choice==3)){
+    playerscore++;
+    pcelections(choice);
+    victoria ="ganaste!"
+    log(playerchoice,choice, victoria);
+    round++;
+    
+}
+    else if ((playerchoice=='piedra'&&choice==1)
+    ||(playerchoice=='papel'&&choice==2)||(playerchoice==3&&choice==3)){
         console.log("it's a tie")
+        pcelections(choice);
+        
+        round++;
+        victoria="es un empate!"
+        log(playerchoice,choice,victoria);
     }
     else {console.log('you lost!!');
     pcscore++;
+    round++
+    pcelections(choice);
+    victoria="perdiste!"
+    log(playerchoice,choice,victoria);
+    
 }
 }
+function fround(){
+    if (round==5){
+       seleccuerpo.style.display="none";
+       if (pcscore>playerscore){
+        seleccoment.textContent=`tu tienes ${playerscore} puntos y la pc tiene ${pcscore} puntos, perdiste`
+   } else if(pcscore<playerscore){
+    seleccoment.textContent=`tu tienes ${playerscore} puntos y la pc tiene ${pcscore} puntos, ganaste`
+   }
+    } 
 
-let playerChoice=prompt("choose rock-paper-scisors");
-
-let choice=computerSelection(1,3);
+}
 
 
 
@@ -33,11 +120,13 @@ let choice=computerSelection(1,3);
 
 
 
-console.log (playerChoice,choice)
 
-playmech(playerChoice,choice)
 
-function pcelections (){
+
+
+
+
+function pcelections (choice){
     let election;
     if (choice==1){
     return election="rock"
@@ -50,12 +139,20 @@ function pcelections (){
 
 
 
-
-    console.log ("you choose "+playerChoice+" and the pc choose "+pcelections(choice))
-    
-    
-
-
+function log(playerchoice,choice,victoria){
+    if (choice==1){
+        seleccoment.textContent=`tu eliges ${playerchoice} y la pc elige piedra, ${victoria}`;
+    }
+    else if(choice==2){
+        seleccoment.textContent=`tu eliges ${playerchoice} y la pc elige papel, ${victoria}`;
+    }
+    else if(choice==3){
+        seleccoment.textContent=`tu eliges ${playerchoice} y la pc elige tijeras, ${victoria}`;
+    }
 }
 
-console.log("your score is: "+playerscore+" and the pc score is "+pcscore)
+
+
+
+
+//console.log("your score is: "+playerscore+" and the pc score is "+pcscore)
